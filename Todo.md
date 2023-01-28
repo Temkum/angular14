acc30c388f00ec066b3a1b76b0c99cf882a0f49c
Kum Jude Tem <judekum14@gmail.com>
2023-01-28 23:15:01 +0100

    Create new component using cli

---
+++ Todo.md
@@ -0,0 +1,14 @@
+# Angular 14
+
+## project structure
+
+- `tsconfig.json ` holds settings for typescript
+- tsconfig.spec.json for testing
+- typescript for dev
+- angular cli for commands
+- `angular.json` is used for cli
+
+## create component
+
+- `ng generate component component-name`
+- remember to add the component name in the module `app.module.ts`
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CakesComponent } from './cakes/cakes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CakesComponent
   ],
   imports: [
     BrowserModule
